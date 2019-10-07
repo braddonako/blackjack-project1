fbde671d0a0071e23e68f2c1a9dbca7303a75dab
Brad Donakowski <braddonako@gmail.com>
2019-10-07 10:43:48 -0600

    Made an array with the cards. Added PNG images for cards

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// 1)create a deck x
+// 2) write a function to shuffle the deck 
+// 3) write a function to deal the cards to each player. 
+// 4) write a function to for hit and stat. Porbably with jquery. If the button is pressed,
+// they will be dealt another card
+// 5) Computer player will automatically hit until they are over 17 or bust
+// 6) We need a function that will reset the game after a round is
+
+const Player1 = {
+	name: 'Brad'
+}
+
+const cardId = [
+'10C',
+'10D',
+'10H',
+'10S',
+'2C',
+'2D',
+'2H',
+'2S',
+'3C',
+'3D',
+'3H',
+'3S',
+'4C',
+'4D',
+'4H',
+'4S',
+'5C',
+'5D',
+'5H',
+'5S',
+'6C',
+'6D',
+'6H',
+'6S',
+'7C',
+'7D',
+'7H',
+'7S',
+'8C',
+'8D',
+'8H',
+'8S',
+'9C',
+'9D',
+'9H',
+'9S',
+'11AC',
+'11AD',
+'11AH',
+'11AS',
+'10JC',
+'10JD',
+'10JH',
+'10JS',
+'10KC',
+'10KD',
+'10KH',
+'10KS',
+'10QC',
+'10QD',
+'10QH',
+'10QS'
+]
+
+
+const shuffle = () => {
+
+}
+
+
+
+
+
+
